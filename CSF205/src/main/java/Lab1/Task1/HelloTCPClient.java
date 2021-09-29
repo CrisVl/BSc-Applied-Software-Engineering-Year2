9b050e07104fb1b020533f6809637468d6684480
criso <>
2021-09-29 17:55:38 +0100

    refractor

--- CSF205/src/main/java/Lab1/Task1/HelloTCPClient.java
+++ CSF205/src/main/java/Lab1/Task1/HelloTCPClient.java
@@ -1,39 +1,21 @@
 package Lab1.Task1;
 
-/**
- * A Simple Java TCP Client Server Program
- * These are used for illustrating how the client server works for CSF205
- * Certain inputs are hardcoded for ease of use.
- * You can modify them as you wish.
- * This program is purposefully done without threads to
- * illustrate the fact that it can be done so too.
- * If you want you can extend this by creating a thread for the server
- * actions.
- */
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.Random;
-
-/**
- * This is the TCP Client code that sends a language in which
- * it wants the word "hello" in and waits for the appropriate
- * response. Doesn't do anything else.
- *
- * @author rama_swuni
- */
 public class HelloTCPClient {
 
     public static void main(String[] args) {
 
-        Random rand = new Random();
-        int n = rand.nextInt(50);
-
         try {
-            Socket clientSocket = new Socket("127.0.0.1", 3939);
+            Socket clientSocket = new Socket("127.0.0.1",3939);
+            InputStream in = clientSocket.getInputStream();
 
-            System.out.println("Response from server: " + n);
-
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String rand = br.readLine();
+            System.out.println(rand);
             clientSocket.close();
 
         } catch (IOException e) {
--- CSF205/src/main/java/Lab1/Task1/HelloTcpServer.java
+++ CSF205/src/main/java/Lab1/Task1/HelloTcpServer.java
@@ -1,45 +1,33 @@
 package Lab1.Task1;
 
-/**
- * A Simple Java TCP Client Server Program
- * These are used for illustrating how the client server works for CSF205
- * Certain inputs are hardcoded for ease of use.
- * You can modify them as you wish.
- * This program is purposefully done without threads to
- * illustrate the fact that it can be done so too.
- * If you want you can extend this by creating a thread for the server
- * actions.
- */
-
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Random;
 
-/**
- * This is the TCP Server code that accepts a language in which
- * the client want the word "hello" in and sends the appropriate
- * response. Doesn't do anything else.
- * @author rama_swuni
- *
- */
 public class HelloTcpServer {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
+
+        Random rand = new Random();
+        int n = rand.nextInt(50);
 
         try {
 
             ServerSocket serverSocket = new ServerSocket(3939);
-            System.out.println("Waiting for a client request...");
+            System.out.println("Waiting for client connection...");
 
             Socket clientSocket = serverSocket.accept();
 
+            OutputStream output = clientSocket.getOutputStream();
+            PrintWriter writer = new PrintWriter(output, true);
+
+            writer.println(n);
             clientSocket.close();
             serverSocket.close();
 
         } catch (IOException e) {
+
             System.out.println(e.getMessage());
         }
 
